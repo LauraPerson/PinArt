f95948aa183214b47b7099df1a09684d074819cf
laura person <lauraperson29800@gmail.com>
2022-05-23 18:14:52 +0200

    added possibility to add event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,32 @@
 class EventsController < ApplicationController
-  def new
-  end
 
   def index
     @events = Event.all
   end
 
   def show
+  end
+
+
+  def new 
+    @event = Event.new
+  end
+
+
+  def create
+    @event = Event.new(params_event)
+    @event.user = current_user
+    if @event.save!
+      redirect_to events_path
+    else
+      redirect_to home_path
+    end
+  end
+
+  private 
+
+  def params_event
+    params.require(:event).permit(:name, :city, :institution, :date)
   end
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  CATEGORIES = ["Festival", "Musée", "Galerie", "Exposition en extérieur"]
+
 end
