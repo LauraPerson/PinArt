daf949decf3c303c698615832521b60588e91184
laura person <lauraperson29800@gmail.com>
2022-06-07 15:48:16 +0200

    front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,23 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
-
-
   def home
-    @owned_tags = current_user.owned_tags
-    @most_used_tags = @owned_tags.most_used(10)
-    @events = Event.all.where(user_id: current_user)
+    if current_user
+      @owned_tags = current_user.owned_tags
+      @tags = @owned_tags.most_used(10)
+      @events_user = Event.all.where(user_id: current_user)
 
-    if params[:query].present?
-      @events = @events.where('name ILIKE?', "%#{params[:query]}%") 
-      @owned_tags = @owned_tags.where('name ILIKE?', "%#{params[:query]}%") 
-    end
+      if params[:query].nil?
+        @events = @events_user.first(5)
+      else
+        @events = @events_user.where('name ILIKE?', "%#{params[:query]}%") 
+        @tags = @owned_tags.where('name ILIKE?', "%#{params[:query]}%") 
+      end
 
-    respond_to do |format|
-      format.html # Follow regular flow of Rails
-      format.text { render partial: "pages/list", locals: {events: @events}, formats: [:html] }
+      respond_to do |format|
+        format.html # Follow regular flow of Rails
+        format.text { render partial: "pages/list", locals: {events: @events}, formats: [:html] }
+      end
     end
   end
 end
